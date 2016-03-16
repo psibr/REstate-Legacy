a7c59269a16317e8666e9df42a54ef0e6556de82
Ovan Crone <ovan@psibr.net>
2016-03-16 13:42:43 -0500

    Replacing bat with ps1

--- DeployDatabase.bat
+++
@@ -1,1 +0,0 @@
-src\packages\FluentMigrator.Tools.1.6.1\tools\AnyCPU\40\Migrate --connectionString REstate --provider SqlServer2014 --target src\REstate.Database.SqlServer\bin\Debug\REstate.Database.SqlServer.dll --tag Full
