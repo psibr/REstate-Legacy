93af140b1b1cba8faa773fe012b935c9bff9c759
Ovan Crone <ovan@psibr.net>
2016-04-18 09:55:42 -0500

    Adding retry policies for rabbit.

--- src/REstate.Connectors.RabbitMq/RabbitMqConnector.cs
+++ src/REstate.Connectors.RabbitMq/RabbitMqConnector.cs
@@ -9,6 +9,7 @@ using Psibr.Platform.Serialization;
 using RabbitMQ.Client;
 using REstate.Configuration;
 using REstate.Services;
+using Polly;
 
 namespace REstate.Connectors.RabbitMq
 {
@@ -40,50 +41,59 @@ namespace REstate.Connectors.RabbitMq
                 if (payload == null)
                     payload = actionSettings.MessageBody;
 
-                using (var connection = ConnectionFactory.CreateConnection())
-                using (var channel = connection.CreateModel())
-                {
-                    var properties = channel.CreateBasicProperties();
-
-                    properties.ContentType = actionSettings.ContentType ?? "application/json";
-                    properties.Type = actionSettings.MessageType;
-                    if (actionSettings.Headers != null)
-                        properties.Headers = actionSettings.Headers
-                            .ToDictionary<KeyValuePair<string, string>,
-                                string, object>(kvp => kvp.Key, kvp => kvp.Value);
-
-                    properties.Headers = properties.Headers ?? new Dictionary<string, object>();
-
-                    properties.Headers.Add("MachineInstanceId", machineInstance.MachineInstanceId);
-                    properties.Headers.Add("MachineDefinitionId", machineInstance.MachineDefinitionId);
-
-                    try
-                    {
-                        channel.BasicPublish(actionSettings.ExchangeName,
-                            actionSettings.MessageType, properties, Encoding.UTF8.GetBytes(payload));
-
+                Policy.Handle<Exception>().WaitAndRetryForever((i) =>
+                    TimeSpan.FromSeconds((System.Math.Floor(15 * (System.Math.Log10(30 * i) * i)) - 7) * (i <= 5 ? 1 : (i - 4))),
+                    (ex, timespan, ctx) =>
                         Logger
-                            .ForContext("ActionConfiguration", actionSettings, true)
-                            .Debug("Successfully published RabbitMq action message. " +
-                                   "MessageType: {messageType}, Payload: {payload}",
-                                   actionSettings.MessageType, payload);
-                    }
-                    catch (Exception ex)
+                            .ForContext("retryContext", ctx)
+                            .Error(ex, "An error occured while attempting to send a RabbitMq message. Will retry after {time} seconds.", timespan.Seconds))
+                    .Execute(() =>
                     {
-                        ex.Data.Add("VirtualHost", ConnectionFactory.VirtualHost);
-                        ex.Data.Add("HostName", ConnectionFactory.HostName);
-                        ex.Data.Add("ExchangeName", actionSettings.ExchangeName);
-                        ex.Data.Add("RoutingKey", properties.Type);
-                        ex.Data.Add("Headers", properties.Headers);
-                        ex.Data.Add("Payload", payload);
+                        using (var connection = ConnectionFactory.CreateConnection())
+                        using (var channel = connection.CreateModel())
+                        {
+                            var properties = channel.CreateBasicProperties();
 
-                        Logger.Error(ex, "Failed to publish action message to RabbitMq. " +
-                                         "MessageType: {messageType}, Payload: {payload}",
-                                         actionSettings.MessageType, payload);
+                            properties.ContentType = actionSettings.ContentType ?? "application/json";
+                            properties.Type = actionSettings.MessageType;
+                            if (actionSettings.Headers != null)
+                                properties.Headers = actionSettings.Headers
+                                    .ToDictionary<KeyValuePair<string, string>,
+                                        string, object>(kvp => kvp.Key, kvp => kvp.Value);
 
-                        throw;
-                    }
-                }
+                            properties.Headers = properties.Headers ?? new Dictionary<string, object>();
+
+                            properties.Headers.Add("MachineInstanceId", machineInstance.MachineInstanceId);
+                            properties.Headers.Add("MachineDefinitionId", machineInstance.MachineDefinitionId);
+
+                            try
+                            {
+                                channel.BasicPublish(actionSettings.ExchangeName,
+                                    actionSettings.MessageType, properties, Encoding.UTF8.GetBytes(payload));
+
+                                Logger
+                                    .ForContext("ActionConfiguration", actionSettings, true)
+                                    .Debug("Successfully published RabbitMq action message. " +
+                                           "MessageType: {messageType}, Payload: {payload}",
+                                           actionSettings.MessageType, payload);
+                            }
+                            catch (Exception ex)
+                            {
+                                ex.Data.Add("VirtualHost", ConnectionFactory.VirtualHost);
+                                ex.Data.Add("HostName", ConnectionFactory.HostName);
+                                ex.Data.Add("ExchangeName", actionSettings.ExchangeName);
+                                ex.Data.Add("RoutingKey", properties.Type);
+                                ex.Data.Add("Headers", properties.Headers);
+                                ex.Data.Add("Payload", payload);
+
+                                Logger.Error(ex, "Failed to publish action message to RabbitMq. " +
+                                                 "MessageType: {messageType}, Payload: {payload}",
+                                                 actionSettings.MessageType, payload);
+
+                                throw;
+                            }
+                        }
+                    });                
 
                 return Task.CompletedTask;
             };
