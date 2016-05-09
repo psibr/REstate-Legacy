160ee25602ed1d29eeec2e544ef8faafd77b1eb3
Ovan Crone <ovan@psibr.net>
2016-05-08 22:43:30 -0500

    fixing helper scripts

--- svc-stop.bat
+++ svc-stop.bat
@@ -1,5 +1,5 @@
-start cmd.exe /C "src\REstate.Services.Auth\bin\Release\REstate.Services.Auth.exe stop"
-start cmd.exe /C "src\REstate.Services.Core\bin\Release\REstate.Services.Core.exe stop"
-start cmd.exe /C "src\REstate.Services.Chrono\bin\Release\REstate.Services.Chrono.exe stop"
-start cmd.exe /C "src\REstate.Services.ChronoConsumer\bin\Release\REstate.Services.ChronoConsumer.exe stop"
-start cmd.exe /C "src\REstate.Service.AdminUI\bin\Release\REstate.Services.AdminUI.exe stop"
+src\REstate.Services.Auth\bin\Release\REstate.Services.Auth.exe stop 
+src\REstate.Services.Core\bin\Release\REstate.Services.Core.exe stop
+src\REstate.Services.Chrono\bin\Release\REstate.Services.Chrono.exe stop
+src\REstate.Services.ChronoConsumer\bin\Release\REstate.Services.ChronoCnsumer.exe stop
+src\REstate.Service.AdminUI\bin\Release\REstate.Services.AdminUI.exe stop
--- svc-uninstall.bat
+++ svc-uninstall.bat
@@ -1,8 +1,8 @@
 
 netsh http delete urlacl url=http://+:80/
 
-start cmd.exe /C "src\REstate.Services.Auth\bin\Release\REstate.Services.Auth.exe uninstall"
-start cmd.exe /C "src\REstate.Services.Core\bin\Release\REstate.Services.Core.exe uninstall"
-start cmd.exe /C "src\REstate.Services.Chrono\bin\Release\REstate.Services.Chrono.exe uninstall"
-start cmd.exe /C "src\REstate.Services.ChronoConsumer\bin\Release\REstate.Services.ChronoConsumer.exe uninstall"
-start cmd.exe /C "src\REstate.Service.AdminUI\bin\Release\REstate.Services.AdminUI.exe uninstall"
+src\REstate.Services.Auth\bin\Release\REstate.Services.Auth.exe uninstall
+src\REstate.Services.Core\bin\Release\REstate.Services.Core.exe uninstall
+src\REstate.Services.Chrono\bin\Release\REstate.Services.Chrono.exe uninstall
+src\REstate.Services.ChronoConsumer\bin\Release\REstate.Services.ChronoCnsumer.exe uninstall
+src\REstate.Service.AdminUI\bin\Release\REstate.Services.AdminUI.exe uninstall
