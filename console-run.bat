6833d906c51645cd398048a147ffef43cf9b69f9
Ovan Crone <ovan@psibr.net>
2016-05-09 00:37:21 -0500

    updates

--- console-run.bat
+++ console-run.bat
@@ -1,3 +1,4 @@
+
 start cmd.exe /C "src\REstate.Services.Auth\bin\Release\REstate.Services.Auth.exe"
 start cmd.exe /C "src\REstate.Services.Core\bin\Release\REstate.Services.Core.exe"
 start cmd.exe /C "src\REstate.Services.Chrono\bin\Release\REstate.Services.Chrono.exe"
--- svc-install.bat
+++ svc-install.bat
@@ -4,5 +4,5 @@ netsh http add urlacl url=http://+:80/ user="NT AUTHORITY\NETWORK SERVICE"
 src\REstate.Services.Auth\bin\Release\REstate.Services.Auth.exe install
 src\REstate.Services.Core\bin\Release\REstate.Services.Core.exe install
 src\REstate.Services.Chrono\bin\Release\REstate.Services.Chrono.exe install
-src\REstate.Services.ChronoConsumer\bin\Release\REstate.Services.ChronoCnsumer.exe install
+src\REstate.Services.ChronoConsumer\bin\Release\REstate.Services.ChronoConsumer.exe install
 src\REstate.Service.AdminUI\bin\Release\REstate.Services.AdminUI.exe install
