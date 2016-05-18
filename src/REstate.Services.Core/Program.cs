5092e412a32b2b5c24b9b397de1d9a7dd1d61192
Ovan Crone <ovan@psibr.net>
2016-05-18 18:39:54 -0500

    Removing connectors.

--- src/REstate.Services.Core/Program.cs
+++ src/REstate.Services.Core/Program.cs
@@ -33,7 +33,7 @@ namespace REstate.Services.Core
             var config = JsonConvert.DeserializeObject<REstatePlatformConfiguration>(configString);
 
             var kernel = BuildAndConfigureContainer(config).Build();
-
+            var x = kernel.Resolve<IEnumerable<IConnectorFactory>>();
             HostFactory.Run(host =>
             {
                 host.UseSerilog(kernel.Resolve<ILogger>());
@@ -118,19 +118,6 @@ namespace REstate.Services.Core
             });
 
             return container;
-        }
-    }
-
-    public static class FluentIfExtension
-    {
-        public static T If<T>(this T fluentObject, Func<T, bool> predicate, Action<T> fluentContinuation)
-        {
-            if (predicate(fluentObject))
-            {
-                fluentContinuation(fluentObject);
-            }
-
-            return fluentObject;
         }
     }
 }
